3c736d171da26970738a0f9ffbd3fb2c25820438
homura <admin@example.com>
2023-06-26 14:58:30 +0000

    build

--- start.sh
+++ start.sh
@@ -3,13 +3,14 @@ gitclone() {
     echo "git clone...."
     # chmod 700 ~/.ssh
     # chmod 600 ~/.ssh/*
-    git clone $repo_url
-    folder_name=$(basename "$repo_url" .git)
-    folder_name=${folder_name##*/}
-    echo "$folder_name"
-    cp -r $folder_name/* .
-    cp -r $folder_name/.* .
-    rm -rf $folder_name
+    git clone $repo_url .
+    # folder_name=$(basename "$repo_url" .git)
+    # folder_name=${folder_name##*/}
+    # echo "$folder_name"
+    # cp -r $folder_name/* .
+    # cp -r $folder_name/.git .
+    # cp -r $folder_name/.gitlab-ci.yml .
+    # rm -rf $folder_name
 }
 if [[ ! -d $hexoInit ]]; then
     touch 'hexoInit'
