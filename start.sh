c0c0728a01e894a24c8fcaae6eaefcfd35780694
homura <943384135@qq.com>
2023-08-19 08:08:25 +0800

    up

--- start.sh
+++ start.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 gitclone() {
     echo "git clone...."
-    # chmod 700 ~/.ssh
-    # chmod 600 ~/.ssh/*
     git clone $repo_url .
-    # folder_name=$(basename "$repo_url" .git)
-    # folder_name=${folder_name##*/}
-    # echo "$folder_name"
-    # cp -r $folder_name/* .
-    # cp -r $folder_name/.git .
-    # cp -r $folder_name/.gitlab-ci.yml .
-    # rm -rf $folder_name
 }
 if [ -z "$(ls -A)" ]; then
     gitclone
