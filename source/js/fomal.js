682c22c4f8c61d001e0c7eef3f91d8a83403ea80
homura <943384135@qq.com>
2023-11-02 16:47:32 +0800

    up

--- source/js/fomal.js
+++ source/js/fomal.js
@@ -292,8 +292,8 @@ function showWelcome() {
   }
 }
 window.onload = showWelcome;
-// // 如果使用了pjax在加上下面这行代码
-// document.addEventListener('pjax:complete', showWelcome);
+// 如果使用了pjax在加上下面这行代码
+document.addEventListener('pjax:complete', showWelcome);
 
 /* 欢迎信息 end */
 
